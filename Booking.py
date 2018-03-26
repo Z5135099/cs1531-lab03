7d75e962d093371bbc23a9650d73da6e60c0eba2
Z5135099 <36779497+Z5135099@users.noreply.github.com>
2018-03-26 11:43:44 +1100

    Add files via upload

--- Booking.py
+++ Booking.py
@@ -11,14 +11,17 @@ class booking():
         self.__car = car
         
     def calculation(self):
-        
-        self.__price = (self.__car.getDailyFee())*self.__period
-        if car.type == 'large':
-            self.__price = self.__price * 1.05
+        if self.__car.getCurrentStatus() is 'available':
+            self.__price = (self.__car.getDailyFee())*self.__period
+            if self.__car.getSize is 'large':
+                self.__price = self.__price * 1.05
+                
+            if self.__car.getType is 'premium':
+                self.__price = self.__price * 1.2 
+        else:
+            return 'Unavaliable car'
             
-        if car.type == 'premium':
-            self.__price = self.__price * 1.2
-        return self.__price    
             
     def __str__(self):
         return "Costumer name:%s\nPick up location:%s\nDrop off location:%s\nCar type:%s\nPeriod:%s\nPrice:%f\n"%(self.__name)(self.__pickUpLocation)(self.__dropOffLocation)(self.__car.getType())(self.__period)(self.__price)
+        
